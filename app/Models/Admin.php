25d45f53e92269f7e810b6ad6b5685b61d116bff
RJ0506 <rafaeljosephzaide06@gmail.com>
2024-10-13 22:45:32 +0800

    feat: basic migration and model for admin

---
+++ app/Models/Admin.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Models;
+
+// use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+
+class Admin extends Authenticatable
+{
+	/** @use HasFactory<\Database\Factories\AdminFactory> */
+	use HasFactory, Notifiable;
+
+	/**
+	 * The attributes that are mass assignable.
+	 *
+	 * @var array<int, string>
+	 */
+
+	protected $table = 'admin';
+
+	protected $fillable = [
+		'first_name',
+		'middle_name',
+		'last_name',
+		'email',
+		'password',
+	];
+
+	/**
+	 * The attributes that should be hidden for serialization.
+	 *
+	 * @var array<int, string>
+	 */
+	protected $hidden = [
+		'password',
+	];
+
+	/**
+	 * Get the attributes that should be cast.
+	 *
+	 * @return array<string, string>
+	 */
+	protected function casts(): array
+	{
+		return [
+			'password' => 'hashed',
+		];
+	}
+}
