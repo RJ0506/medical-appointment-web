5325911267404ec48f81ec95ab5804ecd6e35f42
RJ0506 <rafaeljosephzaide06@gmail.com>
2024-10-14 10:23:33 +0800

    feat: basic login/logout on user module

---
+++ app/Http/Controllers/User/AuthController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Controllers\User;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Auth;
+
+class AuthController extends Controller
+{
+
+	public function login(Request $request)
+	{
+		$request->validate([
+			'email' => 'required|email',
+			'password' => 'required|min:3',
+		]);
+
+		$user = User::where('email', $request->email)->first();
+
+		if (!$user || !Hash::check($request->password, $user->password)) {
+			return response('The provided credentials are incorrect', 401);
+		}
+
+		Auth::login($user);
+
+		$uAuth = auth()->user();
+		$uAuth->tokens()->delete();
+		$token = $uAuth->createToken('user-token')->plainTextToken;
+
+		return response([
+			'user' => $user,
+			'token' => $token
+		]);
+	}
+
+	public function logout()
+	{
+		auth()->user()->tokens()->delete();
+		return response('Successfully logged out!', 200);
+	}
+
+
+	public function me()
+	{
+		return response()->json(auth()->user());
+	}
+}
--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,23 @@
 <?php
 
-use Illuminate\Http\Request;
+use App\Http\Controllers\User\AuthController as UserAuthController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+
+Route::prefix('user')->group(function () {
+	Route::controller(UserAuthController::class)->group(function () {
+		Route::post('/login', 'login');
+
+		Route::middleware(['auth:sanctum'])->group(function () {
+			Route::post('/logout', [UserAuthController::class, 'logout']);
+			Route::post('/me', [UserAuthController::class, 'me']);
+		});
+	});
+
+	Route::middleware(['auth:sanctum'])->group(function () {
+
+	});
+
+});
+
+//
