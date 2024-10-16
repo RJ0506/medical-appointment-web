6f3d2d72de4304627df381c382bddafa1aeb51e5
Dean Zaballero <126765808+Feyded@users.noreply.github.com>
2024-10-16 20:10:29 +0800

    fixed routing

--- vue/pages/patient/index.vue
+++ vue/pages/patient/index.vue
@@ -2,7 +2,7 @@
     <div class="grid min-h-screen place-content-center">
         <div class="flex gap-4 px-2 sm:gap-10">
             <NuxtLink
-                to="patient/medical"
+                to="patient/medical-appointment"
                 class="grid h-[300px] max-w-64 cursor-pointer place-content-center rounded-md bg-[#aac7b8] p-1 text-center shadow-md transition-all duration-500 ease-out hover:-translate-y-3 hover:bg-emerald-400 sm:h-[484px]"
             >
                 <p class="text-lg font-bold text-white sm:text-xl">
@@ -10,7 +10,7 @@
                 </p>
             </NuxtLink>
             <NuxtLink
-                to="patient/dental"
+                to="patient/dental-appointment"
                 class="grid h-[300px] max-w-64 cursor-pointer place-content-center rounded-md bg-[#aac7b8] p-1 text-center shadow-md transition-all duration-500 ease-out hover:-translate-y-3 hover:bg-emerald-400 sm:h-[484px]"
             >
                 <p class="text-lg font-bold text-white sm:text-xl">
