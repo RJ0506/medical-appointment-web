a8162ded0b458fba58f4056da77bb43e862b74b9
RJ0506 <rafaeljosephzaide06@gmail.com>
2024-10-16 15:49:54 +0800

    feat: token middleware on user-token

---
+++ app/Http/Middleware/ValidTokenName.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class ValidTokenName
+{
+	/**
+	 * Handle an incoming request.
+	 *
+	 * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+	 */
+	public function handle(Request $request, Closure $next, string $name): Response
+	{
+		$user = auth()->user();
+		$tokenName = $user->currentAccessToken()->name;
+
+		// Perform checks based on token name
+		if ($tokenName !== $name) {
+			return response()->json(['message' => 'You are unauthorized to make this request.'], 401);
+		}
+
+		return $next($request);
+	}
+}
--- routes/api.php
+++ routes/api.php
@@ -8,16 +8,12 @@ use Illuminate\Support\Facades\Route;
 
 
 Route::prefix('user')->group(function () {
-	Route::controller(UserAuthController::class)->group(function () {
-		Route::post('/login', 'login');
+	Route::post('/login', [UserAuthController::class, 'login']);
 
-		Route::middleware(['auth:sanctum'])->group(function () {
-			Route::post('/logout', [UserAuthController::class, 'logout']);
-			Route::post('/me', [UserAuthController::class, 'me']);
-		});
-	});
+	Route::middleware(['auth:sanctum', 'token_name:user-token'])->group(function () {
+		Route::post('/logout', [UserAuthController::class, 'logout']);
+		Route::post('/me', [UserAuthController::class, 'me']);
 
-	Route::middleware(['auth:sanctum'])->group(function () {
 		Route::resources([
 			'service-types' => ServiceTypeController::class,
 			'service-categories' => ServiceCategoryController::class,
