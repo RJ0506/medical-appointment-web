796496aa13ce5e3357e8538ad39cc9ebedb639f0
Dean Zaballero <126765808+Feyded@users.noreply.github.com>
2024-10-27 10:07:25 +0800

    patient history

--- vue/pages/user/dental-health-record/[id].vue
+++ vue/pages/user/dental-health-record/[id].vue
@@ -1,6 +1,6 @@
-<!-- <template>
-    <div class="mx-auto mt-16 max-w-7xl">
-        <div class="flex flex-wrap">
+<template>
+    <div class="mt-16 max-w-7xl mx-auto">
+        <div class="flex">
             <NuxtLink
                 class="rounded-md border border-black bg-[#a6a6a6] px-4 py-2 font-bold text-black hover:bg-[#d9d9d9]"
                 to="medical-health-record"
@@ -8,7 +8,7 @@
             >
             <NuxtLink
                 class="rounded-md border border-black bg-[#a6a6a6] px-4 py-2 font-bold text-black hover:bg-[#d9d9d9]"
-                to="medical-health-record/health-history"
+                to="#"
                 >Health History</NuxtLink
             >
             <NuxtLink
@@ -24,12 +24,10 @@
         </div>
         <div>
             <div>
-                <form class="grid gap-4 bg-[#d9d9d9] px-4 py-8">
+                <div class="grid gap-4 bg-[#d9d9d9] px-4 py-8">
                     <div class="flex flex-col items-center justify-center">
                         <img src="/tua-logo.png" alt="logo" />
-                        <h1 class="text-2xl uppercase">
-                            Trinity University of Asia
-                        </h1>
+                        <h1 class="uppercase text-2xl">Trinity University of Asia</h1>
                         <p>Medical and Dental Unit</p>
                     </div>
                     <h2 class="bg-[#a6a6a6] text-center font-bold">
@@ -207,12 +205,7 @@
                             </div>
                         </div>
                     </div>
-                    <button
-                        class="bg-emerald-800 py-2 text-white hover:bg-emerald-900"
-                    >
-                        Submit
-                    </button>
-                </form>
+                </div>
             </div>
         </div>
     </div>
@@ -221,96 +214,4 @@
 definePageMeta({
     layout: "user",
 });
-</script> -->
-
-<template>
-    <div class="grid gap-5 py-6">
-        <h1 class="text-xl font-bold sm:text-5xl">Medical Health Record</h1>
-        <!-- SEARCH -->
-        <div class="relative mx-auto mt-7 flex w-fit items-center gap-2">
-            <h2 class="font-semibold">Search:</h2>
-            <input
-                type="text"
-                v-model="searchTerm"
-                class="search-input rounded-md border border-gray-300 pl-4 pr-10 focus:outline-none focus:ring-2"
-                placeholder="ID Number"
-            />
-            <svg
-                class="absolute right-3 top-1/2 h-5 w-5 -translate-y-1/2 transform text-gray-400"
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke-width="2"
-                stroke="currentColor"
-                aria-hidden="true"
-            >
-                <path
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    d="M21 21l-4.35-4.35M17 10a7 7 0 10-14 0 7 7 0 0014 0z"
-                />
-            </svg>
-        </div>
-
-        <!-- TABLE -->
-        <div class="overflow-x-auto">
-            <table class="w-full table-auto text-left">
-                <thead class="text-white">
-                    <tr class="bg-[#1e3d2c]">
-                        <th class="whitespace-nowrap p-5">ID Number</th>
-                        <th class="whitespace-nowrap p-5">Classification</th>
-                        <th class="whitespace-nowrap p-5">Full Name</th>
-                        <th class="whitespace-nowrap p-5">Action</th>
-                    </tr>
-                </thead>
-                <tbody class="whitespace-nowrap">
-                    <tr v-for="(item, index) in filteredRecords" :key="index">
-                        <td class="p-5 font-medium">{{ item.id }}</td>
-                        <td class="p-5 font-medium">
-                            {{ item.classification }}
-                        </td>
-                        <td class="p-5 font-medium">
-                            {{ item.fullname }}
-                        </td>
-                        <td class="flex gap-2 p-5 font-medium">
-                            <NuxtLink
-                                :to="`/user/medical-health-record/${item.id}`"
-                                class="rounded bg-blue-500 px-4 py-2 font-bold text-white hover:bg-blue-700"
-                            >
-                                Edit
-                            </NuxtLink>
-                        </td>
-                    </tr>
-                </tbody>
-            </table>
-        </div>
-    </div>
-</template>
-
-<script setup lang="ts">
-definePageMeta({
-    layout: "user",
-});
-
-const searchTerm = ref("");
-
-const filteredRecords = computed(() => {
-    if (!searchTerm.value) {
-        return records.value;
-    }
-    return records.value.filter((item: any) => {
-        return (
-            item.id.toString().includes(searchTerm.value.toLowerCase()) ||
-            item.fullname.toLowerCase().includes(searchTerm.value.toLowerCase())
-        );
-    });
-});
-
-const records = ref([
-    { id: 1, classification: "Student", fullname: "John Doe" },
-    { id: 2, classification: "Employee", fullname: "Jane Smith" },
-    { id: 3, classification: "Student", fullname: "Bob Johnson" },
-    { id: 4, classification: "Employee", fullname: "Alice Brown" },
-    { id: 5, classification: "Student", fullname: "Mike Davis" },
-]);
 </script>
--- vue/pages/user/dental-health-record/index.vue
+++ vue/pages/user/dental-health-record/index.vue
@@ -1,6 +1,6 @@
-<template>
-    <div class="mt-16 max-w-7xl mx-auto">
-        <div class="flex">
+<!-- <template>
+    <div class="mx-auto mt-16 max-w-7xl">
+        <div class="flex flex-wrap">
             <NuxtLink
                 class="rounded-md border border-black bg-[#a6a6a6] px-4 py-2 font-bold text-black hover:bg-[#d9d9d9]"
                 to="medical-health-record"
@@ -8,7 +8,7 @@
             >
             <NuxtLink
                 class="rounded-md border border-black bg-[#a6a6a6] px-4 py-2 font-bold text-black hover:bg-[#d9d9d9]"
-                to="#"
+                to="medical-health-record/health-history"
                 >Health History</NuxtLink
             >
             <NuxtLink
@@ -24,10 +24,12 @@
         </div>
         <div>
             <div>
-                <div class="grid gap-4 bg-[#d9d9d9] px-4 py-8">
+                <form class="grid gap-4 bg-[#d9d9d9] px-4 py-8">
                     <div class="flex flex-col items-center justify-center">
                         <img src="/tua-logo.png" alt="logo" />
-                        <h1 class="uppercase text-2xl">Trinity University of Asia</h1>
+                        <h1 class="text-2xl uppercase">
+                            Trinity University of Asia
+                        </h1>
                         <p>Medical and Dental Unit</p>
                     </div>
                     <h2 class="bg-[#a6a6a6] text-center font-bold">
@@ -205,7 +207,12 @@
                             </div>
                         </div>
                     </div>
-                </div>
+                    <button
+                        class="bg-emerald-800 py-2 text-white hover:bg-emerald-900"
+                    >
+                        Submit
+                    </button>
+                </form>
             </div>
         </div>
     </div>
@@ -214,4 +221,96 @@
 definePageMeta({
     layout: "user",
 });
+</script> -->
+
+<template>
+    <div class="grid gap-5 py-6">
+        <h1 class="text-xl font-bold sm:text-5xl">Dental Health Record</h1>
+        <!-- SEARCH -->
+        <div class="relative mx-auto mt-7 flex w-fit items-center gap-2">
+            <h2 class="font-semibold">Search:</h2>
+            <input
+                type="text"
+                v-model="searchTerm"
+                class="search-input rounded-md border border-gray-300 pl-4 pr-10 focus:outline-none focus:ring-2"
+                placeholder="ID Number"
+            />
+            <svg
+                class="absolute right-3 top-1/2 h-5 w-5 -translate-y-1/2 transform text-gray-400"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke-width="2"
+                stroke="currentColor"
+                aria-hidden="true"
+            >
+                <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    d="M21 21l-4.35-4.35M17 10a7 7 0 10-14 0 7 7 0 0014 0z"
+                />
+            </svg>
+        </div>
+
+        <!-- TABLE -->
+        <div class="overflow-x-auto">
+            <table class="w-full table-auto text-left">
+                <thead class="text-white">
+                    <tr class="bg-[#1e3d2c]">
+                        <th class="whitespace-nowrap p-5">ID Number</th>
+                        <th class="whitespace-nowrap p-5">Classification</th>
+                        <th class="whitespace-nowrap p-5">Full Name</th>
+                        <th class="whitespace-nowrap p-5">Action</th>
+                    </tr>
+                </thead>
+                <tbody class="whitespace-nowrap">
+                    <tr v-for="(item, index) in filteredRecords" :key="index">
+                        <td class="p-5 font-medium">{{ item.id }}</td>
+                        <td class="p-5 font-medium">
+                            {{ item.classification }}
+                        </td>
+                        <td class="p-5 font-medium">
+                            {{ item.fullname }}
+                        </td>
+                        <td class="flex gap-2 p-5 font-medium">
+                            <NuxtLink
+                                :to="`/user/dental-health-record/${item.id}`"
+                                class="rounded bg-blue-500 px-4 py-2 font-bold text-white hover:bg-blue-700"
+                            >
+                                Edit
+                            </NuxtLink>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    </div>
+</template>
+
+<script setup lang="ts">
+definePageMeta({
+    layout: "user",
+});
+
+const searchTerm = ref("");
+
+const filteredRecords = computed(() => {
+    if (!searchTerm.value) {
+        return records.value;
+    }
+    return records.value.filter((item: any) => {
+        return (
+            item.id.toString().includes(searchTerm.value.toLowerCase()) ||
+            item.fullname.toLowerCase().includes(searchTerm.value.toLowerCase())
+        );
+    });
+});
+
+const records = ref([
+  { id: 6, classification: "Faculty", fullname: "Emily Chen" },
+  { id: 7, classification: "Employee", fullname: "David Lee" },
+  { id: 8, classification: "Student", fullname: "Sarah Taylor" },
+  { id: 9, classification: "Faculty", fullname: "Kevin White" },
+  { id: 10, classification: "Employee", fullname: "Lisa Nguyen" },
+]);
 </script>
