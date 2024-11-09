8595bfe278514ac4fa7f4937eefa611405a91031
RJ0506 <rafaeljosephzaide06@gmail.com>
2024-11-09 16:52:24 +0800

    removed: unused import in departments controller

--- app/Http/Controllers/User/DepartmentController.php
+++ app/Http/Controllers/User/DepartmentController.php
@@ -3,7 +3,6 @@
 namespace App\Http\Controllers\User;
 
 use App\Http\Requests\User\UpsertDepartmentRequest;
-use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Models\Department;
 
