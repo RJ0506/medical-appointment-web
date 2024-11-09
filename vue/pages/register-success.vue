7ff5c390b5060e04f5d6613feac9ff4d7c0e85b2
Dean Zaballero <dean.zaballero@gmail.com>
2024-11-09 19:51:00 +0800

    update label

--- vue/pages/register-success.vue
+++ vue/pages/register-success.vue
@@ -3,7 +3,7 @@
         <h1 class="text-3xl font-bold">Registered successfully!</h1>
         <p class="mt-5 text-lg">
             You can now appoint your schedule in Medical and Dental Unit, LOGIN
-            now using your ID Number and Password.
+            now using your Email and Password.
         </p>
         <NuxtLink
             to="/"
