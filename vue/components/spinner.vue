a833cf0dc9da7a45a343ae32eae4eea96826f439
Feyded <126765808+Feyded@users.noreply.github.com>
2024-11-21 11:52:55 +0800

    adjust height and width of spinner

--- vue/components/spinner.vue
+++ vue/components/spinner.vue
@@ -1,6 +1,11 @@
 <template>
     <div>
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 300 150">
+        <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 300 150"
+            width="150"
+            height="50"
+        >
             <path
                 fill="none"
                 stroke="#347956"
