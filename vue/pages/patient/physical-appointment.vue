43a5adc538c2f03c1a5b32d76051295921ed1300
Dean Zaballero <126765808+Feyded@users.noreply.github.com>
2024-12-19 22:15:27 +0800

    request physical examination

--- vue/pages/patient/physical-appointment.vue
+++ vue/pages/patient/physical-appointment.vue
@@ -1,140 +1,163 @@
 <template>
     <div class="grid">
         <div class="mt-10">
-            <h1 class="text-2xl font-bold sm:text-4xl">Physical Examination Request</h1>
-            <form
-                @submit.prevent="handleSubmit"
-                class="mx-auto mt-8 flex max-w-3xl flex-col gap-2 bg-[#d9d9d9] px-8 py-4"
-            >
-                <h2 class="text-center text-xl font-bold">
-                    Examination Details
-                </h2>
-                <div>
-                    <div class="font-bold">
-                        <h3 for="purpose">Purpose of Examination</h3>
-                        <div class="mt-2">
-                            <input
-                                type="radio"
-                                id="Employment"
-                                name="purpose"
-                                value="Employment"
-                                v-model="formData.purpose"
-                            />
-                            <label for="Employment">Employment</label>
+            <h1 class="text-2xl font-bold sm:text-4xl">
+                Physical Examination Request
+            </h1>
+            <form @submit.prevent="handleSubmit">
+                <div v-if="isAdding">
+                    <Spinner />
+                </div>
+                <div
+                    v-if="!isAdding"
+                    class="mx-auto mt-8 flex max-w-3xl flex-col gap-2 bg-[#d9d9d9] px-8 py-4"
+                >
+                    <h2 class="text-center text-xl font-bold">
+                        Examination Details
+                    </h2>
+                    <div>
+                        <div class="font-bold">
+                            <h3 for="purpose">Purpose of Examination</h3>
+                            <div class="mt-2">
+                                <input
+                                    type="radio"
+                                    id="Employment"
+                                    name="purpose"
+                                    value="Employment"
+                                    v-model="formData.purpose"
+                                />
+                                <label for="Employment">Employment</label>
+                            </div>
+                            <div>
+                                <input
+                                    type="radio"
+                                    id="School"
+                                    name="purpose"
+                                    value="School Requirement"
+                                    v-model="formData.purpose"
+                                />
+                                <label for="School">School Requirement</label>
+                            </div>
+                            <div>
+                                <input
+                                    type="radio"
+                                    id="Sport"
+                                    name="purpose"
+                                    value="Sport Activity"
+                                    v-model="formData.purpose"
+                                />
+                                <label for="Sport">Sports Activity</label>
+                            </div>
+                            <div>
+                                <input
+                                    type="radio"
+                                    id="others"
+                                    name="purpose"
+                                    value="others"
+                                    v-model="formData.purpose"
+                                />
+                                <label for="others"
+                                    >Others (Please Specify)</label
+                                >
+                            </div>
+                            <p
+                                v-if="
+                                    submitErrorMessages &&
+                                    submitErrorMessages.purpose
+                                "
+                                class="text-red-500"
+                            >
+                                {{ submitErrorMessages.purpose[0] }}
+                            </p>
                         </div>
-                        <div>
+                        <div class="mt-5 flex flex-col font-bold">
+                            <label class="font-bold" for="preffered_date"
+                                >Preffered Date of Examination</label
+                            >
                             <input
-                                type="radio"
-                                id="School"
-                                name="purpose"
-                                value="School Requirement"
-                                v-model="formData.purpose"
+                                class="mx-auto mt-2 sm:w-1/3"
+                                type="date"
+                                v-model="formData.scheduled_date"
+                                :min="currentDate"
                             />
-                            <label for="School">School Requirement</label>
+                            <p
+                                v-if="
+                                    submitErrorMessages &&
+                                    submitErrorMessages.scheduled_date
+                                "
+                                class="text-red-500"
+                            >
+                                {{ submitErrorMessages.scheduled_date[0] }}
+                            </p>
                         </div>
-                        <div>
-                            <input
-                                type="radio"
-                                id="Sport"
-                                name="purpose"
-                                value="Sport Activity"
-                                v-model="formData.purpose"
-                            />
-                            <label for="Sport">Sports Activity</label>
+                        <div class="mt-5 font-bold">
+                            <h3 class="font-bold">Preferred Time Slot</h3>
+                            <div>
+                                <input
+                                    type="radio"
+                                    id="Morning"
+                                    name="preffered_time_slot"
+                                    value="Morning"
+                                    v-model="formData.time_slot"
+                                />
+                                <label for="Morning">Morning</label>
+                            </div>
+                            <div>
+                                <input
+                                    type="radio"
+                                    id="Afternoon"
+                                    name="preffered_time_slot"
+                                    value="Afternoon"
+                                    v-model="formData.time_slot"
+                                />
+                                <label for="Afternoon">Morning</label>
+                            </div>
+                            <div>
+                                <input
+                                    type="radio"
+                                    id="Evening"
+                                    name="preffered_time_slot"
+                                    value="Evening"
+                                    v-model="formData.time_slot"
+                                />
+                                <label for="Evening">Evening</label>
+                            </div>
+                            <p
+                                v-if="
+                                    submitErrorMessages &&
+                                    submitErrorMessages.time_slot
+                                "
+                                class="text-red-500"
+                            >
+                                {{ submitErrorMessages.time_slot[0] }}
+                            </p>
                         </div>
-                        <div>
-                            <input
-                                type="radio"
-                                id="others"
-                                name="purpose"
-                                value="others"
-                                v-model="formData.purpose"
-                            />
-                            <label for="others">Others (Please Specify)</label>
+                        <div class="mt-2 text-center font-bold">
+                            <p class="mt-2 text-xl">Authorization</p>
+                            <span
+                                >I, the undersigned, hereby request a physical
+                                examination and authorize the healthcare
+                                provider to perform the necessary examination
+                                and provide a report
+                            </span>
+                            <div class="mt-5 flex justify-center gap-2">
+                                <input
+                                    type="checkbox"
+                                    v-model="formData.isAuthorized"
+                                />
+                                <label for="isAuthorized">I Agree</label>
+                            </div>
                         </div>
-                        <p
-                            v-if="
-                                submitErrorMessages &&
-                                submitErrorMessages.purpose
-                            "
-                            class="text-red-500"
-                        >
-                            {{ submitErrorMessages.purpose[0] }}
-                        </p>
                     </div>
-                    <div class="mt-5 flex flex-col">
-                        <label class="font-bold" for="preffered_date"
-                            >Preffered Date of Examination</label
-                        >
-                        <input
-                            class="mx-auto mt-2 sm:w-1/3"
-                            type="date"
-                            v-model="formData.scheduled_date"
-                            :min="currentDate"
-                        />
-                        <p
-                            v-if="
-                                submitErrorMessages &&
-                                submitErrorMessages.scheduled_date
-                            "
-                            class="text-red-500"
-                        >
-                            {{ submitErrorMessages.scheduled_date[0] }}
-                        </p>
-                    </div>
-                    <div class="mt-5 font-bold">
-                        <h3 class="font-bold">Preferred Time Slot</h3>
-                        <div>
-                            <input
-                                type="radio"
-                                id="Morning"
-                                name="preffered_time_slot"
-                                value="Morning"
-                                v-model="formData.time_slot"
-                            />
-                            <label for="Morning">Morning</label>
-                        </div>
-                        <div>
-                            <input
-                                type="radio"
-                                id="Afternoon"
-                                name="preffered_time_slot"
-                                value="Afternoon"
-                                v-model="formData.time_slot"
-                            />
-                            <label for="Afternoon">Morning</label>
-                        </div>
-                        <div>
-                            <input
-                                type="radio"
-                                id="Evening"
-                                name="preffered_time_slot"
-                                value="Evening"
-                                v-model="formData.time_slot"
-                            />
-                            <label for="Evening">Evening</label>
-                        </div>
-                        <p
-                            v-if="
-                                submitErrorMessages &&
-                                submitErrorMessages.time_slot
-                            "
-                            class="text-red-500"
-                        >
-                            {{ submitErrorMessages.time_slot[0] }}
-                        </p>
-                    </div>
-                    <div class="mt-2 text-center font-bold">
-                        <p class="mt-2 text-xl">Authorization</p>
-                        <span
-                            >I, the undersigned, hereby request a physical
-                            examination and authorize the healthcare provider to
-                            perform the necessary examination and provide a
-                            report
-                        </span>
-                    </div>
+                </div>
+                <div v-if="!isAdding" class="mt-5 flex justify-center">
                     <button
-                        class="mt-5 w-fit self-center rounded bg-[#1e3d2c] px-10 py-1 text-white hover:bg-emerald-800"
+                        :disabled="!formData.isAuthorized"
+                        :class="{
+                            'cursor-not-allowed bg-gray-400 text-gray-700': !formData.isAuthorized,
+                            'hover:bg-emerald-800 bg-[#1e3d2c]': formData.isAuthorized,
+                        }"
+                        class="rounded px-10 py-1 text-white "
                     >
                         Submit
                     </button>
@@ -183,6 +206,7 @@ definePageMeta({
 const currentDate = ref("");
 const isAdding = ref(false);
 const submitErrorMessages = ref();
+const dialogRef = ref();
 const authStore = useAuthStore();
 
 const initialFormData = ref({
@@ -205,6 +229,11 @@ const month = String(today.getMonth() + 1).padStart(2, "0");
 const day = String(today.getDate()).padStart(2, "0");
 currentDate.value = `${year}-${month}-${day}`;
 
+const closeDialog = () => {
+    dialogRef.value?.close();
+    navigateTo("/patient");
+};
+
 const handleSubmit = async () => {
     isAdding.value = true;
     submitErrorMessages.value = "";
@@ -220,8 +249,8 @@ const handleSubmit = async () => {
         );
         submitErrorMessages.value = "";
         formData.value = { ...initialFormData.value };
-        // dialogRef.value?.showModal();
         isAdding.value = false;
+        dialogRef.value?.showModal();
     } catch (error) {
         isAdding.value = false;
         submitErrorMessages.value = error.response.data.errors;
@@ -229,3 +258,10 @@ const handleSubmit = async () => {
     }
 };
 </script>
+
+<style scoped>
+::backdrop {
+    backdrop-filter: blur(5px);
+    background-color: rgba(0, 0, 0, 0.4);
+}
+</style>
